3afd31940dc63041771d8aaedf52599cf938d6fc
Krzysztof Janiszewski <krzysztof.andrzej.janiszewski@gmail.com>
2015-12-31 00:47:40 +0000

    Logging added.

--- UiHost/App.xaml.cs
+++ UiHost/App.xaml.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.ComponentModel.Composition.Hosting;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using log4net.Config;
-using UiHost;
 
 namespace UiHost
 {
@@ -34,6 +27,7 @@ namespace UiHost
 
 		public App()
 		{
+			DispatcherUnhandledException += UnhandledException;
 			/*var catalog = new AssemblyCatalog(System.Reflection.Assembly.GetExecutingAssembly());
 			var container = new CompositionContainer(catalog);
 			container.SatisfyImportsOnce(this);
@@ -41,6 +35,23 @@ namespace UiHost
 			//_synthesizer.Value.Say("A B C");
 			_window.Show();*/
 			//new Bootstrapper().Run();
+		}
+
+		private void UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs dispatcherUnhandledExceptionEventArgs)
+		{
+			dispatcherUnhandledExceptionEventArgs.Handled = true;
+			try
+			{
+				var logger = log4net.LogManager.GetLogger(typeof (App));
+				logger.Error(dispatcherUnhandledExceptionEventArgs.Exception);
+			}
+			catch (Exception e)
+			{
+				// Log4net might be not initialized.
+				Console.WriteLine(e);
+				Console.WriteLine(dispatcherUnhandledExceptionEventArgs.Exception);
+			}
+			MessageBox.Show(dispatcherUnhandledExceptionEventArgs.Exception.ToString(), "Unhandled exception");
 		}
 	}
 }
